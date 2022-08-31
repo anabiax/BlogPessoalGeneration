d3f7ec3077e34ff027f8cacbaea4c57d7a472e9d
Ana Santos <anabia200113@gmail.com>
2022-08-31 15:54:09 -0300

    verbos

--- src/main/java/com/generation/blog/controller/PostagemController.java
+++ src/main/java/com/generation/blog/controller/PostagemController.java
@@ -3,9 +3,15 @@ package com.generation.blog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +25,42 @@ import com.generation.blog.repository.PostagemRepository;
 public class PostagemController {
 
 	@Autowired
-	private PostagemRepository postagemRepository;
+	private PostagemRepository repository;
 	
 	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll(){
-		return ResponseEntity.ok(postagemRepository.findAll());
+	public ResponseEntity<List<Postagem>> getAll(){ // retorna todos os Objetos da Classe Postagem persistidos no Banco de dados
+		return ResponseEntity.ok(repository.findAll());
+	
 	}
+	
+	@GetMapping("/{id}") // mapeia todas as Requisições HTTP GET, , enviadas para um endereço específico (Endpoint), dentro do Recurso Postagem, para um Método específico que responderá as requisições, ou seja, ele indica que o Método getById
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){ // variável do caminho
+		return repository.findById(id)
+/*optional*/	.map(resposta -> ResponseEntity.ok(resposta)) //pode devolver tanto um obj ou erro na requisição
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+				// como se fosse uma subrota
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo)); // o parâmetro é o nome da variável
+	}
+	
+	@PostMapping                  //nome do método
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem ){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping                  
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem ){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+	
 }
-
 
--- src/main/java/com/generation/blog/repository/PostagemRepository.java
+++ src/main/java/com/generation/blog/repository/PostagemRepository.java
@@ -1,12 +1,18 @@
 package com.generation.blog.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.generation.blog.model.Postagem;
+
+//Uma interface em Java é uma Classe Abstrata (uma Classe que serve de modelo para outras Classes), composta somente por Métodos abstratos. 
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-// Uma interface em Java é uma Classe Abstrata (uma Classe que serve de modelo para outras Classes), composta somente por Métodos abstratos. 
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
+	
+
 	
 }
